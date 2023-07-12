0592099c8101ffbc831fdcb767bea80a0eeb75c4
Freen <susanasarfo1011@gmali.com>
2023-07-12 09:13:44 +0000

    this is a simple python code
    about python if command

---
+++ exam.py
@@ -0,0 +1,8 @@
+name = "Blackpink" and "Bts"
+name = input("enter your favorite kpop band: ")
+if name == "Blackpink":
+    print("you have a great taste in music")
+elif name == "Bts":
+    print("Not a bad taste tho")
+else:
+    print("Sorry do not have a great taste in music")
