60e32e517716502e500afb4771dc4b3367242cbf
Susan-West <134638730+Susan-West@users.noreply.github.com>
2023-07-12 09:22:45 +0000

    Update exam.py

--- exam.py
+++ exam.py
@@ -1,8 +1,10 @@
-name = "Blackpink" and "Bts"
+name = "Blackpink" and "Bts" and "Newjeans"
 name = input("enter your favorite kpop band: ")
 if name == "Blackpink":
     print("you have a great taste in music")
 elif name == "Bts":
     print("Not a bad taste tho")
+elif name == "Newjeans":
+    print("rookies but great")
 else:
     print("Sorry do not have a great taste in music")
